2b0a2136a14b132810ab7774e394c6da3c4bcef0
Yuniel Garcia <garciayuni84@gmail.com>
2018-05-08 17:49:26 -0400

    working on uploading img

--- minerals/urls.py
+++ minerals/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='list'),
+    url(r'^(?P<pk>\d+)$', views.course_detail, name='detail'),
+
 ]
--- minerals/views.py
+++ minerals/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from minerals.models import Mineral
 
 
 def index(request):
     minerals_list = Mineral.objects.all()
     return render(request, 'minerals/index.html', {"minerals": minerals_list})
+
+
+def course_detail(request, pk):
+    mineral = get_object_or_404(Mineral, pk=pk)
+    return render(request, 'minerals/mineral_detail.html', {'mineral': mineral})
